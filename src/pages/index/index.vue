1f37ef4f1e63e44867f0aeb47403fe93b335c700
276636806@qq.com <276636806@qq.com>
2021-03-19 10:28:31 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,54 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view>
+			<text class="title">{{title}}</text>
+		</view>
+		<swipe />
+	</view>
+</template>
+
+<script>
+import swipe from '@/components/swipe-3d/swipe-3d'
+	export default {
+		components: {
+			swipe
+		},
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin: 200rpx auto 50rpx auto;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
